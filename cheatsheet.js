f0e05a61f5bf4144daa2baf4bd4acf89194b2681
CaryACTvF <31970190+CaryACTvF@users.noreply.github.com>
2020-11-12 08:01:05 -0500

    Create cheatsheet.js

---
+++ cheatsheet.js
@@ -0,0 +1,19 @@
+// Unit 3 - Lesson 1
+// Storing variables
+// Input and Output through Pop Ups
+
+// To store something in the computer memory
+// Use this whenever you create a new variable:
+let variable_name = thing_to_be_stored;
+
+// Changing a variable
+variable_name = new_value;
+
+// Output from Pop Up:
+alert("Whatever you want to say goes here!");
+
+// Get Input from a Pop Up:
+prompt("Your question goes here?")
+
+// To ask a question and store the answer in memory
+let name = prompt("What is your name?");
