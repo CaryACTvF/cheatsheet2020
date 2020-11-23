112887dcb408abf5dae06f0573e5f12b1d906777
CaryACTvF <31970190+CaryACTvF@users.noreply.github.com>
2020-11-23 07:56:28 -0500

    Update cheatsheet.js

--- cheatsheet.js
+++ cheatsheet.js
@@ -17,3 +17,20 @@ prompt("Your question goes here?")
 
 // To ask a question and store the answer in memory
 let name = prompt("What is your name?");
+
+// For selecting an element:
+// Replace the phrase "CSS Selector" with a CSS selector for the object you are selecting. Leave the quotes.
+let variable_name = document.querySelector("CSS Selector");
+
+// For creating an event listener
+// Make sure to copy all 4 lines, from element to });!
+// Replace element with the element that is being assigned the event listener. Replace "event type" with the type of "event", for example "click". Add your procedure in the space.
+element.addEventListener("event_type", function() {
+	// Your procedure goes here.
+
+});
+
+// For playing an audio element;
+// Replace sound with the variable for your sound element.
+sound.currentTime = 0;
+sound.play();
